d9cec7e31189a2e874d817198586bd39a9d6e938
UnrealN1K <73508290+UnrealN1K@users.noreply.github.com>
2020-11-02 19:06:01 -0800

    initial code
    Initial Chess Stuff

--- Chess/Chess/main.cpp
+++ Chess/Chess/main.cpp
@@ -1,18 +1,27 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+using namespace std;
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::Sprite shape;
+    //shape.setFillColor(sf::Color::Green);
+
+    sf::Texture pog;
+    pog.loadFromFile("board.png");
+    shape.setTexture(pog);
 
     while (window.isOpen())
     {
+        //poll for events (While loop constantly runs)
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+            cout << sf::Mouse::getPosition().x << " " << sf::Mouse::getPosition().y << endl;
+
         }
 
         window.clear();
