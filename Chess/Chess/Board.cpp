35e5ede4d60362240e7325458f824b35f8abb9f5
Placeholder9134 <nascardrivergonewrong@gmail.com>
2020-11-09 19:02:39 -0800

    Added board constructor, remove function, and move function.

--- Chess/Chess/Board.cpp
+++ Chess/Chess/Board.cpp
@@ -4,3 +4,23 @@
 Board::Board() {
 	cout << "hello" << endl;
 }
+class Board
+{
+private:
+	Piece* board[8][8];
+public: 
+	Board() {
+		//use a loop to set every spot in the board to a null pointer
+		for (int i = 0; i < 8; i++) {
+			for (int z = 0; z < 8; z++) {
+				board[i][z] = nullptr;
+			}
+		}
+	}
+};
+void remove() {
+	
+}
+void move(int column, int row) {
+
+}
