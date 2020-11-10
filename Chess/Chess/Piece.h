2505107197200d53eb579bbd42b1fdb97bb0fe7c
UnrealN1K <73508290+UnrealN1K@users.noreply.github.com>
2020-11-09 19:03:10 -0800

    Piece Class
    Made Changes to Piece Class

---
+++ Chess/Chess/Piece.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Piece {
+private: 
+	int x;
+	int y;
+	string type;
+	string color;
+public:
+	Piece(string n, string color, int col, int row) {
+		string = n;
+		int x = col;
+		int y = row;
+		string = black
+	}
+};
+
