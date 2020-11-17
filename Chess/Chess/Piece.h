634c12fea098ac2e23379a84b361454d297a4863
UnrealN1K <73508290+UnrealN1K@users.noreply.github.com>
2020-11-16 18:59:48 -0800

    Pawn Start
    Started the pawn moves

--- Chess/Chess/Piece.h
+++ Chess/Chess/Piece.h
@@ -10,11 +10,30 @@ private:
 	string type;
 	string color;
 public:
-	Piece(string n, string color, int col, int row) {
-		string = n;
+	Piece(string n, string ColorX, int col, int row) {
+		type = n;
 		int x = col;
 		int y = row;
-		string = black
+		color = ColorX;
+	}
+	bool isValidMove(int col, int row, Board X) {
+		if (type == "pawn") {
+			if (color == "black") {
+
+				if (x == 6) {
+					row == row - 1;
+					row == row - 2;
+				}
+			}
+			else {
+				if (x == 1) {
+					row == row + 1;
+					row == row + 2;
+				}
+			}
+		}
+
+
 	}
 };
 
