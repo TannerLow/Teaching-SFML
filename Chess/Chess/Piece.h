65eb710aedcc229e976c0b1d8ac695bbeae19346
UnrealN1K <73508290+UnrealN1K@users.noreply.github.com>
2020-12-07 19:03:45 -0800

    Pawn
    We did the pawn

--- Chess/Chess/Piece.h
+++ Chess/Chess/Piece.h
@@ -23,43 +23,49 @@ public:
 	bool isValidMove(int col, int row, Board &boardy) {
 		if (type == "pawn") {
 			if (color == "black") {
-				if (col == x - 1 and row == y - 1) { // and enemy piece 
+				if (col == x - 1 and row == y - 1 and boardy.isEnemy(row, col, color)) { // and enemy piece 
 					return true;
 				}
-				if (col == x + 1 and row == y - 1) { // and enemy piece 
+				if (col == x + 1 and row == y - 1 and boardy.isEnemy(row, col, color)) { // and enemy piece 
 					return true;
 				}
-				if (col == x and row == y - 1) { // and no enemy piece
+				if (col == x and row == y - 1 and !boardy.isEnemy(row, col, color) and !boardy.isAlly(row, col, color)) { // and no enemy piece
 					return true;
 				}
-				if (col == x and row == 4 and x == 6) {
+				if (col == x and row == 4 and y == 6 and !boardy.isEnemy(row, col, color) and !boardy.isAlly(row, col, color) and !boardy.isEnemy(row + 1, col, color) and !boardy.isAlly(row + 1, col, color)) {
 				 // Check if there any pieces in front of it, 1 or 2 spots
 					return true;
+				
+					
 				}
 			}
 			else {
-				if (x == 1) {
-					// if pawn is white 
+				if (col == x + 1 and row == y + 1 and boardy.isEnemy(row, col, color)) { // and enemy piece 
+					return true;
 				}
+				if (col == x - 1 and row == y + 1 and boardy.isEnemy(row, col, color)) { // and enemy piece 
+					return true;
+				}
+				if (col == x and row == y + 1 and !boardy.isEnemy(row, col, color) and !boardy.isAlly(row, col, color)) { // and no enemy piece
+					return true;
+				}
+				if (col == x and row == 3 and y == 1 and !boardy.isEnemy(row, col, color) and !boardy.isAlly(row, col, color) and !boardy.isEnemy(row - 1, col, color) and !boardy.isAlly(row - 1, col, color)) {
+					// Check if there any pieces in front of it, 1 or 2 spots
+					return true;
 			}
+				
+			
 		}
-		if (type == "king") {
-			if (color == "black") { //color
-				if (y - 1 <= row and row <= y + 1) {
-					if (x - 1 <= col and col <= x + 1) {
-						if (col != x and row != y) // can't land on same piece 
-							return true;
-					}
-				}
-			}  
-			if (color == "white") { //color
+		if (type == "king") {  
 				if (y - 1 <= row and row <= y + 1) {
 					if (x - 1 <= col and col <= x + 1) {
 						if (col != x and row != y) // can't land on same piece
 							return true;
+						if (boardy.isAlly(row, col, color) == true) {
+							return false;
+						}
 					}
-			  }
-			}
+			    }
 		}
 		if (type == "Rook") {
 			//If the row and column are both changed, the function will see it as an invalid move
