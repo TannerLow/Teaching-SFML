9a5de9766cdc001f8a069c9bc956eb95c7878e8e
Placeholder9134 <nascardrivergonewrong@gmail.com>
2020-12-07 19:02:17 -0800

    Added the queen file and changed main.cpp
    Queen was added to isValidMove and main.cpp was changed. Everything was removed, and a simple user input was added.

--- Chess/Chess/main.cpp
+++ Chess/Chess/main.cpp
@@ -1,33 +1,11 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include "Board.h"
+using namespace std;
 
-int main()
-{
-    using namespace std;
-    sf::RenderWindow window(sf::VideoMode(900, 900), "SFML works!");
-    sf::Sprite shape;
-    sf::Texture texture;
-    texture.loadFromFile("board.png");
-    shape.setTexture(texture);
+int main() {
+	char c(100);
 
-    while (window.isOpen())
-    {
-        //poll for events (While loop constantly runs)
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-            else if (event.type == sf::Event::MouseButtonReleased); {
-                cout << sf::Mouse::getPosition().x << " " << sf::Mouse::getPosition().y << endl;
-            }
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
-
-    return 0;
+	cin >> c;
+	cout << c;
 }
