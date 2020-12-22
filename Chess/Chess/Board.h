c4d331b2104c8baf89b01dd5a9c065f3ed59dffb
Placeholder9134 <nascardrivergonewrong@gmail.com>
2020-12-21 18:49:26 -0800

    Added the deconstructor and new piece stuff.

--- Chess/Chess/Board.h
+++ Chess/Chess/Board.h
@@ -6,9 +6,13 @@ using namespace std;
 class Board
 {
 private:
-	Piece* board[8][8];
+	Piece*** board;
 public:
 	Board() {
+		board = new Piece * *[8];
+		for (int i; i < 8; i++)
+			board[i] = new Piece *[8];
+
 		//use a loop to set every spot in the board to a null pointer
 		for (int i = 0; i < 8; i++) {
 			for (int z = 0; z < 8; z++) {
@@ -17,25 +21,25 @@ public:
 		}
 		for (int i = 0; i < 8; i++) {
 			board[1][i] = new Piece("pawn", "white", i, 1);
-			board[6][i] = new Piece("pawn", "black", i, 1);
+			board[6][i] = new Piece("pawn", "black", i, 6);
 		}
-		board[0][7] = new Piece("rook", "white", 0, 7);
+		board[0][7] = new Piece("rook", "white", 7, 0);
 		board[0][0] = new Piece("rook", "white", 0, 0);
-		board[0][1] = new Piece("knight", "white", 0, 1);
-		board[0][6] = new Piece("knight", "white", 0, 6);
-		board[0][2] = new Piece("bishop", "white", 0, 2);
-		board[0][5] = new Piece("bishop", "white", 0, 5);
-		board[0][3] = new Piece("king", "white", 0, 3);
-		board[0][4] = new Piece("queen", "white", 0, 4);
+		board[0][1] = new Piece("knight", "white", 1, 0);
+		board[0][6] = new Piece("knight", "white", 6, 0);
+		board[0][2] = new Piece("bishop", "white", 2, 0);
+		board[0][5] = new Piece("bishop", "white", 5, 0);
+		board[0][3] = new Piece("king", "white", 3, 0);
+		board[0][4] = new Piece("queen", "white", 4, 0);
 
-		board[6][7] = new Piece("rook", "black", 6, 7);
-		board[6][0] = new Piece("rook", "black", 6, 0);
-		board[6][1] = new Piece("knight", "black", 6, 1);
-		board[6][6] = new Piece("knight", "black", 6, 6);
-		board[6][2] = new Piece("bishop", "black", 6, 2);
-		board[6][5] = new Piece("bishop", "black", 6, 5);
-		board[6][3] = new Piece("king", "black", 6, 3);
-		board[6][4] = new Piece("queen", "black", 6, 4);
+		board[7][7] = new Piece("rook", "black", 7, 7);
+		board[7][0] = new Piece("rook", "black", 0, 7);
+		board[7][1] = new Piece("knight", "black", 1, 7);
+		board[7][6] = new Piece("knight", "black", 6, 7);
+		board[7][2] = new Piece("bishop", "black", 2, 7);
+		board[7][5] = new Piece("bishop", "black", 5, 7);
+		board[7][3] = new Piece("king", "black", 3, 7);
+		board[7][4] = new Piece("queen", "black", 4, 7);
 
 	}
 	void remove(int r, int c) {
@@ -43,7 +47,7 @@ public:
 		board[r][c] = nullptr;
 	}
 	void move(int original_row, int original_column, int new_row, int new_column) {
-		if (board[original_row][original_column]->isValidMove(new_column, new_row, *this)) {
+		if (board[original_row][original_column]->isValidMove(new_column, new_row, board)) {
 			if (board[new_row][new_column] != nullptr) {
 				remove(new_row, new_column);
 				board[original_row][original_column] = board[new_row][new_column];
@@ -55,16 +59,15 @@ public:
 			}
 		}
 	}
-	bool isEnemy(int row, int column, string color) {
-		if (board[row][column] != nullptr and color != board[row][column]->getColor()) {
-			return true;
+	~Board() {
+		for (int i; i < 8; i++) {
+			for (int b; i < 8; i++) {
+				remove(i, b);
+			}
 		}
-		return false;
+		for (int i; i < 8; i++)
+			delete[] board[i];
+		delete[] board;
 	}
-	bool isAlly(int row, int column, string color) {
-		if (board[row][column] != nullptr and color == board[row][column]->getColor()) {
-			return true;
-		}
-		return false;
-	}
+
 };
--- Chess/Chess/Piece.h
+++ Chess/Chess/Piece.h
@@ -20,7 +20,7 @@ public:
 	string getColor () {
 		return color;
 	}
-	bool isValidMove(int col, int row, Board &boardy) {
+	bool isValidMove(int col, int row, Piece*** boardy) {
 		if (type == "pawn") {
 			if (color == "black") {
 				if (col == x - 1 and row == y - 1 and boardy.isEnemy(row, col, color)) { // and enemy piece 
@@ -33,10 +33,10 @@ public:
 					return true;
 				}
 				if (col == x and row == 4 and y == 6 and !boardy.isEnemy(row, col, color) and !boardy.isAlly(row, col, color) and !boardy.isEnemy(row + 1, col, color) and !boardy.isAlly(row + 1, col, color)) {
-				 // Check if there any pieces in front of it, 1 or 2 spots
+					// Check if there any pieces in front of it, 1 or 2 spots
 					return true;
-				
-					
+
+
 				}
 			}
 			else {
@@ -52,11 +52,11 @@ public:
 				if (col == x and row == 3 and y == 1 and !boardy.isEnemy(row, col, color) and !boardy.isAlly(row, col, color) and !boardy.isEnemy(row - 1, col, color) and !boardy.isAlly(row - 1, col, color)) {
 					// Check if there any pieces in front of it, 1 or 2 spots
 					return true;
+				}
+
+
 			}
-				
-			
-		}
-		if (type == "king") {  
+			if (type == "king") {
 				if (y - 1 <= row and row <= y + 1) {
 					if (x - 1 <= col and col <= x + 1) {
 						if (col != x and row != y) // can't land on same piece
@@ -65,220 +65,221 @@ public:
 							return false;
 						}
 					}
-			    }
-		}
-		if (type == "Knight") {
-			if (col == x + 1 and row == y + 2 and !boardy.isAlly(row, col, color))
-				return true;
-			if (col == x + 1 and row == y - 2 and !boardy.isAlly(row, col, color))
-				return true;
-			if (col == x + 2 and row == y + 1 and !boardy.isAlly(row, col, color))
-				return true;
-			if (col == x + 2 and row == y - 1 and !boardy.isAlly(row, col, color)) 
-				return true;
-			if (col == x - 1 and row == y - 1 and !boardy.isAlly(row, col, color))
-				return true;
-			if (col == x - 1 and row == y + 2 and !boardy.isAlly(row, col, color))
-				return true;
-			if (col == x - 2 and row == y - 1 and !boardy.isAlly(row, col, color))
-				return true;
-			if (col == x - 2 and row == y + 1 and !boardy.isAlly(row, col, color))
-				return true;
-			return false;
-		}
-		if (type == "Rook") {
-			//If the row and column are both changed, the function will see it as an invalid move
-			if (x != col and y == row or x == col and y != row) { 
-				if (x < col and y == row) { // to the right
-					for (int i = 1; col - i > x; i++) {
-						if (boardy.isEnemy(row, col - i, color)) {
-							return false;
-						}
-						else if (boardy.isAlly(row, col - i, color)) {
-							return false;
-						}
-					}
 				}
-				else if (x > col and y == row) { // to the left
-					for (int i = 1; col + i < x; i++) {
-						if (boardy.isEnemy(row, col + i, color)) {
-							return false;
-						}
-						else if (boardy.isAlly(row, col + i, color)) {
-							return false;
-						}
-					}
-				}
-				else if (x == col and y > row) { // going down
-					for (int i = 1; row - i > y; i++) {
-						if (boardy.isEnemy(row - i, col, color)) {
-							return false;
-						}
-						else if (boardy.isAlly(row - i, col, color)) {
-							return false;
-						}
-					}
-				}
-				else if (x == col and y < row) { // going up
-					for (int i = 1; row + i < y; i++) {
-						if (boardy.isEnemy(row + i, col, color)) {
-							return false;
-						}
-						else if (boardy.isAlly(row + i, col, color)) {
-							return false;
-						}
-					}
-				}
-				if (boardy.isAlly(row, col, color)) {
-					return false;
-				}
-				return true;
 			}
-		}
-		if (type == "Bishop") {
-			//If the row and column are both changed, the function will see it as an invalid move
-			if (x != col and y != row) {
-				if (x < col and y < row) { // going top right
-					for (int i = 1; col - i > x and row - i > y; i++) {
-						if (boardy.isEnemy(row - i, col - i, color)) {
-							return false;
-						}
-						else if (boardy.isAlly(row - i, col - i, color)) {
-							return false;
-						}
-					}
-				}
-				else if (x > col and y > row) { // going bottom left
-					for (int i = 1; col + i < x and row + i < y; i++) {
-						if (boardy.isEnemy(row + i, col + i, color)) {
-							return false;
-						}
-						else if (boardy.isAlly(row + i, col + i, color)) {
-							return false;
-						}
-					}
-				}
-				else if (x < col and y > row) { // going bottom right
-					for (int i = 1; row - i > y and col + i < x; i++) {
-						if (boardy.isEnemy(row - i, col + i, color)) {
-							return false;
-						}
-						else if (boardy.isAlly(row - i, col + i, color)) {
-							return false;
-						}
-					}
-				}
-				else if (x > col and y < row) { // going top left
-					for (int i = 1; row + i < y and col - i > x; i++) {
-						if (boardy.isEnemy(row + i, col - i, color)) {
-							return false;
-						}
-						else if (boardy.isAlly(row + i, col - i, color)) {
-							return false;
-						}
-					}
-				}
-				if (boardy.isAlly(row, col, color)) {
-					return false;
-				}
-				return true;
+			if (type == "Knight") {
+				if (col == x + 1 and row == y + 2 and !boardy.isAlly(row, col, color))
+					return true;
+				if (col == x + 1 and row == y - 2 and !boardy.isAlly(row, col, color))
+					return true;
+				if (col == x + 2 and row == y + 1 and !boardy.isAlly(row, col, color))
+					return true;
+				if (col == x + 2 and row == y - 1 and !boardy.isAlly(row, col, color))
+					return true;
+				if (col == x - 1 and row == y - 1 and !boardy.isAlly(row, col, color))
+					return true;
+				if (col == x - 1 and row == y + 2 and !boardy.isAlly(row, col, color))
+					return true;
+				if (col == x - 2 and row == y - 1 and !boardy.isAlly(row, col, color))
+					return true;
+				if (col == x - 2 and row == y + 1 and !boardy.isAlly(row, col, color))
+					return true;
+				return false;
 			}
-		}
-		if (type == "Queen") {
-			if (x != col and y == row or x == col and y != row) { // rook check
-				if (x < col and y == row) { // to the right
-					for (int i = 1; col - i > x; i++) {
-						if (boardy.isEnemy(row, col - i, color)) {
-							return false;
-						}
-						else if (boardy.isAlly(row, col - i, color)) {
-							return false;
-						}
-					}
-				}
-				else if (x > col and y == row) { // to the left
-					for (int i = 1; col + i < x; i++) {
-						if (boardy.isEnemy(row, col + i, color)) {
-							return false;
-						}
-						else if (boardy.isAlly(row, col + i, color)) {
-							return false;
+			if (type == "Rook") {
+				//If the row and column are both changed, the function will see it as an invalid move
+				if (x != col and y == row or x == col and y != row) {
+					if (x < col and y == row) { // to the right
+						for (int i = 1; col - i > x; i++) {
+							if (boardy.isEnemy(row, col - i, color)) {
+								return false;
+							}
+							else if (boardy.isAlly(row, col - i, color)) {
+								return false;
+							}
 						}
 					}
-				}
-				else if (x == col and y > row) { // going down
-					for (int i = 1; row - i > y; i++) {
-						if (boardy.isEnemy(row - i, col, color)) {
-							return false;
-						}
-						else if (boardy.isAlly(row - i, col, color)) {
-							return false;
-						}
-					}
-				}
-				else if (x == col and y < row) { // going up
-					for (int i = 1; row + i < y; i++) {
-						if (boardy.isEnemy(row + i, col, color)) {
-							return false;
-						}
-						else if (boardy.isAlly(row + i, col, color)) {
-							return false;
+					else if (x > col and y == row) { // to the left
+						for (int i = 1; col + i < x; i++) {
+							if (boardy.isEnemy(row, col + i, color)) {
+								return false;
+							}
+							else if (boardy.isAlly(row, col + i, color)) {
+								return false;
+							}
 						}
 					}
+					else if (x == col and y > row) { // going down
+						for (int i = 1; row - i > y; i++) {
+							if (boardy.isEnemy(row - i, col, color)) {
+								return false;
+							}
+							else if (boardy.isAlly(row - i, col, color)) {
+								return false;
+							}
+						}
+					}
+					else if (x == col and y < row) { // going up
+						for (int i = 1; row + i < y; i++) {
+							if (boardy.isEnemy(row + i, col, color)) {
+								return false;
+							}
+							else if (boardy.isAlly(row + i, col, color)) {
+								return false;
+							}
+						}
+					}
+					if (boardy.isAlly(row, col, color)) {
+						return false;
+					}
+					return true;
 				}
-				if (boardy.isAlly(row, col, color)) {
-					return false;
-				}
-				return true;
 			}
-			else if (x != col and y != row) { // bishop check
-				if (x < col and y < row) { // going top right
-					for (int i = 1; col - i > x and row - i > y; i++) {
-						if (boardy.isEnemy(row - i, col - i, color)) {
-							return false;
-						}
-						else if (boardy.isAlly(row - i, col - i, color)) {
-							return false;
-						}
-					}
-				}
-				else if (x > col and y > row) { // going bottom left
-					for (int i = 1; col + i < x and row + i < y; i++) {
-						if (boardy.isEnemy(row + i, col + i, color)) {
-							return false;
-						}
-						else if (boardy.isAlly(row + i, col + i, color)) {
-							return false;
+			if (type == "Bishop") {
+				//If the row and column are both changed, the function will see it as an invalid move
+				if (x != col and y != row) {
+					if (x < col and y < row) { // going top right
+						for (int i = 1; col - i > x and row - i > y; i++) {
+							if (boardy.isEnemy(row - i, col - i, color)) {
+								return false;
+							}
+							else if (boardy.isAlly(row - i, col - i, color)) {
+								return false;
+							}
 						}
 					}
-				}
-				else if (x < col and y > row) { // going bottom right
-					for (int i = 1; row - i > y and col + i < x; i++) {
-						if (boardy.isEnemy(row - i, col + i, color)) {
-							return false;
-						}
-						else if (boardy.isAlly(row - i, col + i, color)) {
-							return false;
-						}
-					}
-				}
-				else if (x > col and y < row) { // going top left
-					for (int i = 1; row + i < y and col - i > x; i++) {
-						if (boardy.isEnemy(row + i, col - i, color)) {
-							return false;
-						}
-						else if (boardy.isAlly(row + i, col - i, color)) {
-							return false;
+					else if (x > col and y > row) { // going bottom left
+						for (int i = 1; col + i < x and row + i < y; i++) {
+							if (boardy.isEnemy(row + i, col + i, color)) {
+								return false;
+							}
+							else if (boardy.isAlly(row + i, col + i, color)) {
+								return false;
+							}
 						}
 					}
+					else if (x < col and y > row) { // going bottom right
+						for (int i = 1; row - i > y and col + i < x; i++) {
+							if (boardy.isEnemy(row - i, col + i, color)) {
+								return false;
+							}
+							else if (boardy.isAlly(row - i, col + i, color)) {
+								return false;
+							}
+						}
+					}
+					else if (x > col and y < row) { // going top left
+						for (int i = 1; row + i < y and col - i > x; i++) {
+							if (boardy.isEnemy(row + i, col - i, color)) {
+								return false;
+							}
+							else if (boardy.isAlly(row + i, col - i, color)) {
+								return false;
+							}
+						}
+					}
+					if (boardy.isAlly(row, col, color)) {
+						return false;
+					}
+					return true;
 				}
-				if (boardy.isAlly(row, col, color)) {
-					return false;
-				}
-				return true;
 			}
+			if (type == "Queen") {
+				if (x != col and y == row or x == col and y != row) { // rook check
+					if (x < col and y == row) { // to the right
+						for (int i = 1; col - i > x; i++) {
+							if (boardy.isEnemy(row, col - i, color)) {
+								return false;
+							}
+							else if (boardy.isAlly(row, col - i, color)) {
+								return false;
+							}
+						}
+					}
+					else if (x > col and y == row) { // to the left
+						for (int i = 1; col + i < x; i++) {
+							if (boardy.isEnemy(row, col + i, color)) {
+								return false;
+							}
+							else if (boardy.isAlly(row, col + i, color)) {
+								return false;
+							}
+						}
+					}
+					else if (x == col and y > row) { // going down
+						for (int i = 1; row - i > y; i++) {
+							if (boardy.isEnemy(row - i, col, color)) {
+								return false;
+							}
+							else if (boardy.isAlly(row - i, col, color)) {
+								return false;
+							}
+						}
+					}
+					else if (x == col and y < row) { // going up
+						for (int i = 1; row + i < y; i++) {
+							if (boardy.isEnemy(row + i, col, color)) {
+								return false;
+							}
+							else if (boardy.isAlly(row + i, col, color)) {
+								return false;
+							}
+						}
+					}
+					if (boardy.isAlly(row, col, color)) {
+						return false;
+					}
+					return true;
+				}
+				else if (x != col and y != row) { // bishop check
+					if (x < col and y < row) { // going top right
+						for (int i = 1; col - i > x and row - i > y; i++) {
+							if (boardy.isEnemy(row - i, col - i, color)) {
+								return false;
+							}
+							else if (boardy.isAlly(row - i, col - i, color)) {
+								return false;
+							}
+						}
+					}
+					else if (x > col and y > row) { // going bottom left
+						for (int i = 1; col + i < x and row + i < y; i++) {
+							if (boardy.isEnemy(row + i, col + i, color)) {
+								return false;
+							}
+							else if (boardy.isAlly(row + i, col + i, color)) {
+								return false;
+							}
+						}
+					}
+					else if (x < col and y > row) { // going bottom right
+						for (int i = 1; row - i > y and col + i < x; i++) {
+							if (boardy.isEnemy(row - i, col + i, color)) {
+								return false;
+							}
+							else if (boardy.isAlly(row - i, col + i, color)) {
+								return false;
+							}
+						}
+					}
+					else if (x > col and y < row) { // going top left
+						for (int i = 1; row + i < y and col - i > x; i++) {
+							if (boardy.isEnemy(row + i, col - i, color)) {
+								return false;
+							}
+							else if (boardy.isAlly(row + i, col - i, color)) {
+								return false;
+							}
+						}
+					}
+					if (boardy.isAlly(row, col, color)) {
+						return false;
+					}
+					return true;
+				}
+			}
+
 		}
- 
 	}
 };
 
