4b09d0b168acb1180255fbd08fa4de964f8dc108
UnrealN1K <73508290+UnrealN1K@users.noreply.github.com>
2021-01-18 18:07:16 -0800

    Sup
    Sup

--- Chess/Chess/Renderer.h
+++ Chess/Chess/Renderer.h
@@ -5,16 +5,20 @@ class Renderer
 {
 private:
 	sf::Texture boardImage;
+	sf::Texture piece;
 	sf::Sprite boardSprite;
-
+	sf::Sprite Boardpiece;
 public:
 	Renderer() {
 		boardImage.loadFromFile("board.png");
+		piece.loadFromFile("chess pieces.png");
 		boardSprite.setTexture(boardImage);
+		Boardpiece.setTexture(piece);
 	}
 
 	void draw(sf::RenderWindow& window) {
 		window.draw(boardSprite);
+		window.draw(Boardpiece);
 	}
 
 };
